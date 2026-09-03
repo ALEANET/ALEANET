7e9b9257931c469d40664fea3af921acd4a12599
zeinab-sudo <aboukheir.zb@gmail.com>
2026-09-03 21:51:00 +0200

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap'; // 1. Importez le plugin
 
 export default defineConfig({
-  plugins: [react()],
-  // Important pour GitHub Pages, y compris si le dépôt n'est pas à la racine du domaine.
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://github.io', // 2. Configurez votre URL finale
+      readable: true // Optionnel : rend le fichier XML plus lisible à l'œil humain
+    })
+  ],
   base: "/",
 });
