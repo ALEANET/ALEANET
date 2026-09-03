8d09c13c5fd3234e676942703254ebd1b1db098a
zeinab-sudo <aboukheir.zb@gmail.com>
2026-09-03 21:20:25 +0200

    adress web

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Important pour GitHub Pages, y compris si le dépôt n'est pas à la racine du domaine.
-  base: "/ALEANET/",
+  base: "/",
 });
